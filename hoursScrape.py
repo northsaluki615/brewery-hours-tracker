04c4f70d0c352cdbf5d424126103f764ec210340
Mike <helmers.mike@gmail.com>
2022-02-04 16:29:24 -0600

    Update hoursScrape.py

--- hoursScrape.py
+++ hoursScrape.py
@@ -1,4 +1,17 @@
 # For scraping brewery hours
+#https://opensource.com/article/21/9/web-scraping-python-beautiful-soup
 
+import requests
+import urllib.request
+import time
+from bs4 import BeautifulSoup
+import pprint
 
 url = "https://www.workingdraftbeer.com/" # For testing only
+# uses "Footer-business-hours"
+
+soup = BeautifulSoup(requests.get(url).content, 'html.parser')
+
+results = soup.find('div', {'class': 'Footer-business-hours'})
+
+print(results)
